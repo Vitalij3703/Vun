c64ade428c912d02d2b401cb253e0112602e3b44
Vitalij3703 <v67010168@gmail.com>
2025-09-02 23:39:07 +0300

    added ast (wip)

--- src/include/ast.hpp
+++ src/include/ast.hpp
@@ -1,2 +1,16 @@
 #include <iostream>
+#include <string>
+// dont mind all the token stuff being in the lexer file
+#include "lexer.hpp"
 // in the works
+class ast {
+    public:
+        struct var{
+            string id;
+            auto val;
+        };
+        struct var{
+            
+        };
+        
+}

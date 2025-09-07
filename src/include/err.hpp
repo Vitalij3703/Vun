88b7ddf131836f313a1290631139eda69f083e7c
Vitalij3703 <v67010168@gmail.com>
2025-09-07 09:38:54 +0300

    removed posit from err.hpp and included it to the ast for the future

--- src/include/err.hpp
+++ src/include/err.hpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "lexer.hpp"
 using namespace std;
-struct posit{
-    int line; int col;
-};
+
 class ParseError {
     ParseError(posit pos){
         cerr << "ParseError at pos " << "{"<<"line "<<pos.line<<" collum "<<pos.col<<"}" <<endl;

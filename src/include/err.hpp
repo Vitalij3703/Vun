42bf69748b579dc0d73bb7a213dff31a25837a67
Vitalij3703 <v67010168@gmail.com>
2025-09-13 23:18:32 +0300

    fixed some major errors

--- src/include/err.hpp
+++ src/include/err.hpp
@@ -3,11 +3,13 @@
 using namespace std;
 // i dont really plan on improving this but maybe with enough thinking ill do
 class ParseError {
+    public:
     ParseError(tok token){
         cerr << "ParseError at token: "<<rtv(token)<<endl;
     }
 };
 class RuntimeError {
+    public:
     RuntimeError(){
         cerr << "RuntimeError. please find what caused it."<<endl;
     }

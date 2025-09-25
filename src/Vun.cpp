423e783c697a17d75d11ce5f6adb1ab6b76cdffb
Vitalij3703 <v67010168@gmail.com>
2025-09-25 21:37:32 +0300

    Update Vun.cpp

--- src/Vun.cpp
+++ src/Vun.cpp
@@ -1,16 +1,15 @@
-// main vun file
+
 #include "include/runtime.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
-int main(int argc, char *argv[]){
+int main(int argc, char **argv[]){
     std::fstream file(argv[1]);
-    int i{0};
     std::string buffer;
-    while (!(fstream::cur == '\0')){
-        file.seekg(i);
-        i++;buffer += fstream::cur;
+    while (std::getline(file, buffer)){
+        //idk do something
     }
+    file.close();
     runtime run = runtime(buffer);
     return 0;
 }
